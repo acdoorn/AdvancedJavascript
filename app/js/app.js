e6be811889b9bdb4c203d3ee6d8afb59b1193482
Ruud van Daelen <rdaelen@student.avans.nl>
2015-04-28 21:54:23 +0200

    static user for create and join game

--- app/js/app.js
+++ app/js/app.js
@@ -4,5 +4,7 @@ require('angular/angular');
 // Create your app
 var app = angular.module('AdvJS', []);
 var gameController = require('../controllers/GameController');
+var userController = require('../controllers/userController');
 
 app.controller('GameController', gameController);
+app.controller('UserController', userController);
