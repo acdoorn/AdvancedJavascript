f5b7ef11bffe1144d6d44404891a13ac50efc0e8
acdoorn <acdoorn@student.avans.nl>
2015-05-20 14:35:14 +0200

    Fixed api call

--- app/controllers/GameController.js
+++ app/controllers/GameController.js
@@ -1,222 +1,48 @@
 // GameController.js
-module.exports = function($scope) {
-	this.games = [
-		{
-		 layout: "Snake", // -> 'shanghai'|'snake'|'ox'|'ram'|'dragon'|'rooster'|'monkey'
-		 createdOn: "26-4-2015 21:15:23", // date + time
-		 startedOn: "27-4-2015 18:55:41", // date + time
-		 endedOn: "", // date + time
-		 createdBy: {
-		   id: "rdaelen", // Avans username
-		   name: "Ruud van Daelen", // fullname
-		   email: "rdaelen@avans.nl", // avans e-mail
-		   nickname: "R v D 4" // maybe filled later?
-		 },
-		 minPlayers: "4", // 35 <= x >= 1, Required number of players to start
-		 maxPlayers: "8",  // 35 <= x >= 1
-		 players: [{
-		   id: "rdaelen", // Avans username
-		   name: "Ruud van Daelen", // fullname
-		   email: "rdaelen@avans.nl", // avans e-mail
-		   nickname: "R v D 4" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "acdoorn", // Avans username
-		   name: "Alexander van Doorn", // fullname
-		   email: "acdoorn@avans.nl", // avans e-mail
-		   nickname: "Acdoorn" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "smdhaas", // Avans username
-		   name: "Sander de Haas", // fullname
-		   email: "sm.dehaas@avans.nl", // avans e-mail
-		   nickname: "SanderDeHaas" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "jphes", // Avans username
-		   name: "Jelle van Es", // fullname
-		   email: "jphes@avans.nl", // avans e-mail
-		   nickname: "JelleVanEs" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 } 
-		 ],
-		 state: "playing" // -> 'open'|'playing'|'finished'
-		},
-		{
-		 layout: "Ram", // -> 'shanghai'|'snake'|'ox'|'ram'|'dragon'|'rooster'|'monkey'
-		 createdOn: "25-3-2015 21:15:23", // date + time
-		 startedOn: "30-3-2015 19:41:55", // date + time
-		 endedOn: "15-4-2015 14:33:02", // date + time
-		 createdBy: {
-		   id: "acdoorn", // Avans username
-		   name: "Alexander van Doorn", // fullname
-		   email: "acdoorn@avans.nl", // avans e-mail
-		   nickname: "Acdoorn" // maybe filled later?
-		 },
-		 minPlayers: "2", // 35 <= x >= 1, Required number of players to start
-		 maxPlayers: "6",  // 35 <= x >= 1
-		 players: [{
-		   id: "acdoorn", // Avans username
-		   name: "Alexander van Doorn", // fullname
-		   email: "acdoorn@avans.nl", // avans e-mail
-		   nickname: "Acdoorn" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "rdaelen", // Avans username
-		   name: "Ruud van Daelen", // fullname
-		   email: "rdaelen@avans.nl", // avans e-mail
-		   nickname: "R v D 4" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "smdhaas", // Avans username
-		   name: "Sander de Haas", // fullname
-		   email: "sm.dehaas@avans.nl", // avans e-mail
-		   nickname: "SanderDeHaas" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "jphes", // Avans username
-		   name: "Jelle van Es", // fullname
-		   email: "jphes@avans.nl", // avans e-mail
-		   nickname: "JelleVanEs" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 } 
-		 ],
-		 state: "finished" // -> 'open'|'playing'|'finished'
-		},
-		{
-		 layout: "Dragon", // -> 'shanghai'|'snake'|'ox'|'ram'|'dragon'|'rooster'|'monkey'
-		 createdOn: "25-4-2015 21:15:23", // date + time
-		 startedOn: "", // date + time
-		 endedOn: "", // date + time
-		 createdBy: {
-		   id: "acdoorn", // Avans username
-		   name: "Alexander van Doorn", // fullname
-		   email: "acdoorn@avans.nl", // avans e-mail
-		   nickname: "Acdoorn" // maybe filled later?
-		 },
-		 minPlayers: "6", // 35 <= x >= 1, Required number of players to start
-		 maxPlayers: "16",  // 35 <= x >= 1
-		 players: [{
-		   id: "acdoorn", // Avans username
-		   name: "Alexander van Doorn", // fullname
-		   email: "acdoorn@avans.nl", // avans e-mail
-		   nickname: "Acdoorn" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "rdaelen", // Avans username
-		   name: "Ruud van Daelen", // fullname
-		   email: "rdaelen@avans.nl", // avans e-mail
-		   nickname: "R v D 4" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "smdhaas", // Avans username
-		   name: "Sander de Haas", // fullname
-		   email: "sm.dehaas@avans.nl", // avans e-mail
-		   nickname: "SanderDeHaas" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "jphes", // Avans username
-		   name: "Jelle van Es", // fullname
-		   email: "jphes@avans.nl", // avans e-mail
-		   nickname: "JelleVanEs" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "pjglaros", // Avans username
-		   name: "Paul Laros", // fullname
-		   email: "pjg.laros@avans.nl", // avans e-mail
-		   nickname: "PaulLaros" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "rgjvriel", // Avans username
-		   name: "Rick van Riel", // fullname
-		   email: "rgj.vanriel@avans.nl", // avans e-mail
-		   nickname: "RickVanRiel" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 } 
-		 ],
-		 state: "open" // -> 'open'|'playing'|'finished'
-		},
-		{
-		 layout: "Monkey", // -> 'shanghai'|'snake'|'ox'|'ram'|'dragon'|'rooster'|'monkey'
-		 createdOn: "25-4-2015 21:15:23", // date + time
-		 startedOn: "", // date + time
-		 endedOn: "", // date + time
-		 createdBy: {
-		   id: "rgjvriel", // Avans username
-		   name: "Rick van Riel", // fullname
-		   email: "rgj.vanriel@avans.nl", // avans e-mail
-		   nickname: "RickVanRiel" // maybe filled later?
-		 },
-		 minPlayers: "4", // 35 <= x >= 1, Required number of players to start
-		 maxPlayers: "16",  // 35 <= x >= 1
-		 players: [{
-		   id: "rgjvriel", // Avans username
-		   name: "Rick van Riel", // fullname
-		   email: "rgj.vanriel@avans.nl", // avans e-mail
-		   nickname: "RickVanRiel" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "acdoorn", // Avans username
-		   name: "Alexander van Doorn", // fullname
-		   email: "acdoorn@avans.nl", // avans e-mail
-		   nickname: "Acdoorn" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "rdaelen", // Avans username
-		   name: "Ruud van Daelen", // fullname
-		   email: "rdaelen@avans.nl", // avans e-mail
-		   nickname: "R v D 4" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "smdhaas", // Avans username
-		   name: "Sander de Haas", // fullname
-		   email: "sm.dehaas@avans.nl", // avans e-mail
-		   nickname: "SanderDeHaas" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "jphes", // Avans username
-		   name: "Jelle van Es", // fullname
-		   email: "jphes@avans.nl", // avans e-mail
-		   nickname: "JelleVanEs" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 },
-		 {
-		   id: "pjglaros", // Avans username
-		   name: "Paul Laros", // fullname
-		   email: "pjg.laros@avans.nl", // avans e-mail
-		   nickname: "PaulLaros" // maybe filled later?
-		   // Properties like score and isWinner maybe filled later
-		 }		  
-		 ],
-		 state: "open" // -> 'open'|'playing'|'finished'
-		}
-	];
+module.exports = function($scope, $http) {
+	$scope.loading = true;
+    $scope.editMode = false;
+    $scope.this = this;
 	this.gameTypes = ["Shanghai","Snake","Ox","Ram","Dragon","Rooster","Monkey"];
+    
+	$http.get('https://mahjongmayhem.herokuapp.com/games/').success(function (data) {
+	    $scope.this.games = data;
+		$scope.loading = false;
+	})
+    .error(function () {
+        $scope.error = "An Error has occured while loading posts!";
+        $scope.loading = false;
+    });
+ 
+    $scope.toggleEdit = function() {
+        $scope.editMode = !$scope.editMode;
+    };
+ 
+    $scope.save = function() {
+        $http.pull('/api/posts/', $scope.posts).success(function (data) {
+            alert("Saved Successfully!!");
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Saving posts! " + data;
+            $scope.loading = false;
+        });
+    };
 
 	this.addPlayer = function(game, player) {
 		game.players[game.players.length] = player;
 	}
 
 	this.addGame = function(game, player) {
-		var time = new Date();
-		this.games[this.games.length] = 
+		if(game != null) {
+			var time = new Date();
+			layout = game.layout;
+			template = {
+				_id: layout,
+				__v: 0,
+				id: layout
+			}
+			this.games[this.games.length] = 
 			{
-			 layout: game.layout,
+			 gameTemplate: template,
 			 createdOn: time.toString(), 
 			 startedOn: "",
 			 endedOn: "",
@@ -226,5 +52,6 @@ module.exports = function($scope) {
 			 players: [player],
 			 state: "open"
 			}
+		}
 	}
 }
