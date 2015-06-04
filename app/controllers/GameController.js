05f90e4776aad9b0156b321358933a881045e39c
Alexander van Doorn <turboblaster@live.nl>
2015-06-04 11:26:51 +0200

    idk

--- app/controllers/GameController.js
+++ app/controllers/GameController.js
@@ -1,18 +1,9 @@
 // GameController.js
-module.exports = function($scope, $http) {
+module.exports = function($scope, $http, $routeParams) {
 	$scope.loading = true;
     $scope.editMode = false;
     $scope.this = this;
-	this.gameTypes = ["Shanghai","Snake","Ox","Ram","Dragon","Rooster","Monkey"];
-    
-	$http.get('https://mahjongmayhem.herokuapp.com/games/').success(function (data) {
-	    $scope.this.games = data;
-		$scope.loading = false;
-	}).error(function () {
-        alert("An Error has occured while loading posts!");
-        $scope.loading = false;
-    });
-
+    $scope.this.gameId = '5541fc5b1872631100678bb4';
 	$http.get('https://mahjongmayhem.herokuapp.com/games/5541fc5b1872631100678bb4/tiles/').success(function (data) {
 	    $scope.this.tiles = data;
 		$scope.loading = false;
@@ -24,41 +15,4 @@ module.exports = function($scope, $http) {
     $scope.toggleEdit = function() {
         $scope.editMode = !$scope.editMode;
     };
- 
-    $scope.save = function() {
-        $http.pull('/api/posts/', $scope.posts).success(function (data) {
-            alert("Saved Successfully!!");
-        }).error(function (data) {
-            $scope.error = "An Error has occured while Saving posts! " + data;
-            $scope.loading = false;
-        });
-    };
-
-	this.addPlayer = function(game, player) {
-		game.players[game.players.length] = player;
-	}
-
-	this.addGame = function(game, player) {
-		if(game != null) {
-			var time = new Date();
-			layout = game.layout;
-			template = {
-				_id: layout,
-				__v: 0,
-				id: layout
-			}
-			this.games[this.games.length] = 
-			{
-			 gameTemplate: template,
-			 createdOn: time.toString(), 
-			 startedOn: "",
-			 endedOn: "",
-			 createdBy: player,
-			 minPlayers: 1,
-			 maxPlayers: game.maxPlayers,
-			 players: [player],
-			 state: "open"
-			}
-		}
-	}
 }
